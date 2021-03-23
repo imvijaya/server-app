7aca148f34ed1684ed7d18869e25685d8e706cfa
Vijaya Medisetti <vijayalakshmimedisetti21@gmail.com>
2021-03-23 21:01:08 +0100

    server component declarations deleted from modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    
   ],
   imports: [
     BrowserModule,
