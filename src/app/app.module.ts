7d20b1ced75470413b04e70abea7a3046e7bb08b
Vijaya Medisetti <vijayalakshmimedisetti21@gmail.com>
2021-03-23 22:55:42 +0100

    refactoring old code and added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SearchpageComponent } from './pages/searchpage/searchpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
+    SearchpageComponent,
     
   ],
   imports: [
